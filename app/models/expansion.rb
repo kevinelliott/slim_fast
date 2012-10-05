4dce122916059231dc2cb93b1c0cb4f433b88c0c
Kevin Elliott <kevin@welikeinc.com>
2012-10-04 18:45:40 -0700

    More namespace stripping.

---
+++ app/models/expansion.rb
@@ -0,0 +1,6 @@
+class Expansion < ActiveRecord::Base
+  attr_accessible :domain_id, :referrer_url, :remote_addr, :remote_ip
+  attr_accessible :request_url, :short_url_id, :user_agent
+
+  belongs_to :short_url
+end
---
+++ app/models/short_domain.rb
@@ -0,0 +1,6 @@
+class ShortDomain < ActiveRecord::Base
+	attr_accessible :domain, :is_default
+
+	has_many :short_urls
+	has_many :expansions, :through => :short_urls
+end
